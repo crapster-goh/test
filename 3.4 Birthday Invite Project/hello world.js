079cf70db639f6bdce1421be2fae9c78b7cc332b
crapster-goh <crapster_goh@hotmail.com>
2023-11-07 12:01:06 +0800

    Test

---
+++ 3.4 Birthday Invite Project/hello world.js
@@ -0,0 +1,14 @@
+console.log("Start");
+
+const express = require('express');
+const app = express();
+const port = 3000;
+const hostname = '127.0.0.1';
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
