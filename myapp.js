c97af14cea98bab55588bff09a47a59f3802550f
crapster-goh <crapster_goh@hotmail.com>
2023-11-29 15:07:33 +0800

    Add files via upload

--- myapp.js
+++ myapp.js
@@ -3,7 +3,7 @@ console.log("Start");
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Serve static files from the 'public' directory
 app.use(express.static('public'));
